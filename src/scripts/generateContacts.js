8758b0c39448e572ad34a714ca7369a10c081281
Iryna R <deutsche.post15@gmail.com>
2024-11-09 14:37:56 +0100

    do task

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,23 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    let contacts = await readContacts();
+    if (!Array.isArray(contacts)) {
+      contacts = []; // Ініціалізуємо як порожній масив, якщо тип неправильний
+    }
+    for (let i = 0; i < number; i++) {
+      const newContacts = createFakeContact();
+      contacts.push(newContacts);
+    }
+
+    await writeContacts(contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error.message);
+  }
+};
 
 generateContacts(5);
